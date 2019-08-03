cd236254ca6cced8cf6565173f5dfa3f1491d992
Manuel Bugallo <marvinody@gmail.com>
2019-08-03 17:57:31 -0400

    feat: added simple user view for pregame

--- src/components/_pregame.svelte
+++ src/components/_pregame.svelte
@@ -3,7 +3,15 @@
 </script>
 
 <style>
-
+  .room-player-info {
+    margin-top: 10px;
+  }
+  .room-player-info > .title {
+    margin-bottom: 5px;
+  }
+  .host::before {
+    content: "*";
+  }
 </style>
 
 <div class="columns">
@@ -13,6 +21,16 @@
       <h2 class="subtitle is-6">
         {$room.players.length} / {$room.size} players {$room.spectators.length > 0 ? ` + ${$room.spectators.length} spect.` : ''}
       </h2>
+    </div>
+    <div class="room-player-info">
+      <h1 class="title is-4">Players:</h1>
+      <div class="room-player-list columns">
+        {#each $room.players as player (player.id)}
+          <div class="column" class:host={player.id === $room.host.id}>
+            {player.name}
+          </div>
+        {/each}
+      </div>
     </div>
   </div>
   <div class="column">CHAT</div>
