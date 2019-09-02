09cdaba3d537919d6375cd2b81acdbb46a2253ef
Manuel Bugallo <marvinody@gmail.com>
2019-09-02 15:20:48 -0400

    feat: added names to hands and slight styling

--- src/components/game/Hand.svelte
+++ src/components/game/Hand.svelte
@@ -3,18 +3,41 @@
   export let id = "";
   export let name = "";
 
+  import { self } from "../../stores";
+  $: console.log($self, id);
   import Card from "./Card.svelte";
 </script>
 
 <style>
   .hand {
     display: flex;
+    flex-direction: column;
+    padding-top: 30px;
+    border-top: 1px solid black;
+  }
+
+  .name.self {
+    font-weight: bold;
+  }
+  .name.self::after {
+    margin-left: 10px;
+    content: "(you)";
+  }
+
+  .cards {
+    display: flex;
     justify-content: space-around;
+  }
+  .cards > div {
+    cursor: pointer;
   }
 </style>
 
-<div class="hand" alt="{name}'s cards">
-  {#each cards as { color, value }}
-    <Card {color} value={value === 0 ? '' : value} />
-  {/each}
+<div class="hand">
+  <div class="cards">
+    {#each cards as { color, value }}
+      <Card {color} {value} />
+    {/each}
+  </div>
+  <div class="name" class:self={$self.id === id}>{name}</div>
 </div>
